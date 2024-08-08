87942bb7da08c4c89e61521ae6f6ec5813c48e15
rizkyriyadi <87072818+rizkyriyadi@users.noreply.github.com>
2024-08-09 02:44:26 +0700

    fix{routes}

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { NestExpressApplication } from '@nestjs/platform-express';
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.enableCors({
-    origin: 'http://rizkyriyadi.me',
+    origin: 'https://rizkyriyadi.me',
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
